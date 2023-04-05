b259dcff07d5e452de12a22c996785dbda514592
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-04-05 18:31:25 +0900

    [입출력, 사칙연산] BOJ10998 A*B

---
+++ Basic/BOJ10998.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+print(a*b)
+
+# input의 디폴트 자료형은 string. 
+# int()로 형 변환
+
+# 한 줄에 한 번에 입력:
+# a, b = map(int, input().split())
+# 여러 줄에 걸쳐 입력: 
+# a = int(input())
+# b = int(input())
