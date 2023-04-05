abf3385bab4509ac33a1da5d7ccffc0c89ff419c
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-04-05 21:05:17 +0900

    [문자열] 2675 문자열 반복
    - 힌트 사용
    - map으로 형변환 안 하기r,s=input().split()
    - 줄바꿈 안하기
    end=""
    - 안 쪽 반복문 끝나면 새로운 input 받기 전에 줄바꿈 출력

--- Basic/BOJ2675.py
+++ Basic/BOJ2675.py
@@ -4,9 +4,7 @@ for i in range(num):
     r,s=input().split() 
     r=int(r)
     for j in range(len(s)):
-        print(j)
-        #alphabet=s[j-1]
-        for i in range(r):
-            print("야")
+        print(s[j]*r, end="")
+    print()
                    
         
