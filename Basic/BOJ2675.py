c7b0cb7c4965171415281f1ba0d7f5262ef3c3d9
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-04-05 21:00:11 +0900

    ing

---
+++ Basic/BOJ2675.py
@@ -0,0 +1,12 @@
+#2675 문자열 반복
+num=int(input()) 
+for i in range(num):
+    r,s=input().split() 
+    r=int(r)
+    for j in range(len(s)):
+        print(j)
+        #alphabet=s[j-1]
+        for i in range(r):
+            print("야")
+                   
+        
