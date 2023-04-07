06e76577165aa519716ae26d09811dde18cb4028
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-04-07 18:46:44 +0900

    [조건문] 2480 주사위 세 개 Update
    -max() 사용
    - 숫자 2개가 같은 경우 간소화.
    => 결과가 똑같으니깐 합칠 수 있다.

--- Basic/BOJ2480.py
+++ Basic/BOJ2480.py
@@ -1,5 +1,20 @@
 # 2480 주사위 세 개
+a, b, c = map(int, input().split())
+money = 0
 
+if a == b and b == c:  # 모두 같을 때
+    money = 10000+a*1000
+
+elif a == b or a == c:
+    money = 1000+a*100
+elif b == c:
+    money = 1000+c*100
+else:
+    money = max(a, b, c)*100
+
+print(money)
+'''
+나의 첫 풀이
 a, b, c = map(int, input().split())
 money = 0
 
@@ -26,3 +41,7 @@ else:  # 모두 다를 때
     money = biggest_num * 100
 
 print(money)
+'''
+
+# 숏코딩
+# *_,a,b,c=sorted(input());print(['1'+b,c][a<b<c]+'000'[a<c:])
