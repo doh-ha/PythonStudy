b3a52f909241a1add2447afb471401609c4edf5d
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-04-11 18:17:53 +0900

    [정렬] 10817 세 수
    list() 로 묶어주기

--- PythonStudy/Algorithm/BOJ10817.py
+++ PythonStudy/Algorithm/BOJ10817.py
@@ -1,12 +1,8 @@
-
-a, b, c = map(int, input().split())
-array = []
-array.append(a)
-array.append(b)
-array.append(c)
+arr = list(map(int, input().split()))
+# 리스트 함수 => 형변환. 숫자들을 묶어서 리스트로 만들어줌
 
 
-array.sort(reverse=True)
+arr.sort(reverse=True)
 
 
-print(array[1])
+print(arr[1])
